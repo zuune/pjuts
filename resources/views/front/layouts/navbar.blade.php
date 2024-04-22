1181aaa965b3a188cf6e6c6c5461044b329ff232
zuune <76745880+zuune@users.noreply.github.com>
2024-04-22 22:36:20 +0700

    Update navbar.blade.php

--- resources/views/front/layouts/navbar.blade.php
+++ resources/views/front/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
 
     <nav class="bg-white flex h-20 lg:px-36 md:px-16 sm:px-16 max-[600px]:px-7">
         <div class="flex items-center">
-            <img src="{{ asset('images/logo.png') }}" class="h-10" alt="">
+            <img src="/public/images/logo.png" class="h-10" alt="">
             
         </div>
         <div></div>
