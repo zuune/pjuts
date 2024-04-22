e68b8ecf44656514cc6792a78fb42766802a4d1b
zuune <76745880+zuune@users.noreply.github.com>
2024-04-22 21:47:04 +0700

    Update footer.blade.php

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -6,7 +6,7 @@
           <p class="mt-4 text-lg leading-8 text-gray-300">Apakah anda mengalami kendala atau memerlukan bantuan tambahan? Kami tersedia untuk menjawab pertanyaan.</p>
           <div class="mt-6 flex max-w-md gap-x-4">
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6" placeholder="+62 8128228424" disabled>
+            <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6" placeholder="+62 822-3333-3633" disabled>
             <button type="submit" class="flex-none rounded-md bg-cyan-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500">Hubungi</button>
           </div>
         </div>
