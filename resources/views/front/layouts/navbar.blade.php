c4734b107d5859cd604061eb1041010bf1936165
zuune <76745880+zuune@users.noreply.github.com>
2024-04-22 22:40:57 +0700

    Update navbar.blade.php

--- resources/views/front/layouts/navbar.blade.php
+++ resources/views/front/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <header>
 
-    <nav class="bg-white flex h-20 lg:px-36 md:px-16 sm:px-16 max-[600px]:px-7">
+    <nav class="bg-white flex h-20 lg:px-36 md:px-16 sm:px-16 max-[600px]:px-16">
         <div class="flex items-center">
             <img src="/images/logo.png" class="h-10" alt="">
             
