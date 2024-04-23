83c7020ef6ec11c4ae80a2943e973b0339f5ac58
valktive <valktive@gmail.com>
2024-04-24 00:01:05 +0700

    updatel ogo

--- resources/views/front/layouts/navbar.blade.php
+++ resources/views/front/layouts/navbar.blade.php
@@ -1,9 +1,9 @@
 
     <nav class="bg-white flex h-20 lg:px-36 md:px-16 sm:px-16 max-[600px]:px-7">
         <div class="flex items-center justify-between w-full">
-            <img src="/images/logo.png" class="h-10  mr-2" alt="">
+            <img src="images/logo.png" class="h-10  mr-2" alt="">
 
-            <img src="/images/esdm-logo.png" class="h-10" alt="">
+            <img src="images/esdm-logo.png" class="h-10" alt="">
             
         </div>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <img src="{{ asset('images/logo.png') }}" alt="" width="120">
+                    <img src="images/logo.png" alt="" width="120">
                 </a>
             </div>
 
